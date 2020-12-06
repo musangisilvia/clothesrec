b3ee0267461a152687642a1219c5e4fb9a9175f8
musangisilvia <musangisilvia@gmail.com>
2020-12-06 23:32:01 +0300

    removed navbar, used sidebar instead.

--- index.php
+++ index.php
@@ -21,105 +21,50 @@
     <title>Vazzi Me Up | Clothes Recommendation</title>
 </head>
 <body>
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark"> <!-- navbar navbar-expand-lg navbar-dark bg-dark start -->
+    
+    <div class="row">
+        <div class="col-md- profile">
 
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarMain" aria-controls="navbarMain" aria-expanded="false" aria-label="Toggle navigation">
+            <?php 
             
-            <span class="navbar-toggler-icon"></span>
-
-        </button>
-
-        <div class="collapse navbar-collapse" id="navbarMain">
-
-            <a class="navbar-brand" href="#">Vazzi Me Up</a>
-
-            <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
-
-                <li class="nav-item active">
-
-                    <a class="nav-link" href="#"> Explore Categories <i class="fa fa-search"></i><span class="sr-only">(current)</span></a>
-
-                </li>
-
-                <li class="nav-item">
-
-                    <a class="nav-link" href="#"> My Favourites <i class="fa fa-heart"></i> </a>
-
-                </li>
-
-                <li class="nav-item">
-
-                    <a class="nav-link " href="#" tabindex="-1" aria-disabled="true">Popular <i class="fa fa-star"></i> </a>
-
-                </li>
-
-            </ul>
-
-            <form class="form-inline my-2 my-lg-0">
-
-                <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-
-                <button class="btn btn-outline-light my-2 my-sm-0" type="submit">Search</button>
-
-            </form>
+            include('profile.php');
+            
+            ?>
 
         </div>
-
-    </nav> <!-- navbar navbar-expand-lg navbar-dark bg-dark finish -->
-
-    <nav aria-label="breadcrumb"> <!-- breadcrumb start -->
-        <ol class="breadcrumb">
-            <li class="breadcrumb-item active" aria-current="page">Home</li>
-        </ol>
-    </nav> <!-- breadcrumb finish -->
-
-    <div class="container">
-        <div class="row">
-            <div class="col-md-3 profile">
-
-                <h4> User </h4>
-
-                <?php 
-                
-                include('profile.php');
-                
-                ?>
-
-            </div>
-            <div class="col-md-5">
-                <div id="carouselFade" class="carousel slide carousel-fade" data-ride="carousel">
-                    <div class="carousel-inner">
-                        <div class="carousel-item active">
-                            <img src="images/cuteblouse.jpeg" class="d-block w-100 imgCaro" alt="cutetop.jpg">
-                        </div>
-                        <div class="carousel-item">
-                            <img src="images/trouser.jpeg" class="d-block w-100 imgCaro" alt="cutetop.jpg">
-                        </div>
-                        <div class="carousel-item">
-                            <img src="images/shoe.jpeg" class="d-block w-100 imgCaro" alt="cutetop.jpg">
-                        </div>
+        <div class="col-md-3">
+            <div id="carouselFade" class="carousel slide carousel-fade" data-ride="carousel">
+                <div class="carousel-inner">
+                    <div class="carousel-item active">
+                        <img src="images/cuteblouse.jpeg" class="d-block w-100 imgCaro" alt="cutetop.jpg">
                     </div>
-                    <a class="carousel-control-prev" href="#carouselFade" role="button" data-slide="prev">
-                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                        <span class="sr-only">Previous</span>
-                    </a>
-                    <a class="carousel-control-next" href="#carouselFade" role="button" data-slide="next">
-                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                        <span class="sr-only">Next</span>
-                    </a>
+                    <div class="carousel-item">
+                        <img src="images/trouser.jpeg" class="d-block w-100 imgCaro" alt="cutetop.jpg">
+                    </div>
+                    <div class="carousel-item">
+                        <img src="images/shoe.jpeg" class="d-block w-100 imgCaro" alt="cutetop.jpg">
+                    </div>
                 </div>
+                <a class="carousel-control-prev" href="#carouselFade" role="button" data-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Previous</span>
+                </a>
+                <a class="carousel-control-next" href="#carouselFade" role="button" data-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Next</span>
+                </a>
             </div>
-            <div class="col-md-3 sidebar">
+        </div>
+        <div class="col-md-3 sidebar">
 
-                <h4> Categories </h4>
+            <h4> Categories </h4>
 
-                <?php
-                
-                include('sidebar.php');
-                
-                ?>
-                
-            </div>
+            <?php
+            
+            include('sidebar.php');
+            
+            ?>
+            
         </div>
     </div>
 
--- profile.php
+++ profile.php
@@ -1,1 +1,14 @@
-hi 
+<nav class="menu" tabindex="0">
+	<div class="smartphone-menu-trigger"></div>
+  <header class="avatar">
+		<img src="images/musmus.jpg" />
+    <h2> Musangi  </h2>
+  </header>
+	<ul>
+    <li tabindex="0" class="icon-dashboard"><span>Dashboard</span></li>
+    <li tabindex="0" class="icon-customers"><span>Explore</span></li>
+    <li tabindex="0" class="icon-users"><span>Favorites</span></li>
+    <li tabindex="0" class="icon-settings"><span>Popular</span></li>
+    <li tabindex="0" class="icon-settings"><span>Log Out</span></li>
+  </ul>
+</nav>
