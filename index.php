9ad0bb58718e51a215245748643151391a09ad1f
musangisilvia <musangisilvia@gmail.com>
2020-12-07 12:29:53 +0300

    profileviews,header[C

--- index.php
+++ index.php
@@ -12,7 +12,7 @@
 
     <!--Google Fonts CDN-->
     <link rel="preconnect" href="https://fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css2?family=Comfortaa:wght@600&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Comfortaa&family=Sacramento&display=swap" rel="stylesheet">
 
     <!--CSS-->
     <link rel="stylesheet" href="css/style.css">
@@ -21,51 +21,116 @@
     <title>Vazzi Me Up | Clothes Recommendation</title>
 </head>
 <body>
-    
-    <div class="row">
-        <div class="col-md- profile">
+    <div class="col-md-12 logo">
+        <h3 class="logoTitle" style="color:grey; background-color: #f4f4f4; text-align: center; font-family:'Sacramento', cursive; font-size: 50px;"> Vazzi </h3>
+    </div>
 
-            <?php 
+    <div class="container">
+        <div class="row top">
             
-            include('profile.php');
-            
-            ?>
 
-        </div>
-        <div class="col-md-3">
-            <div id="carouselFade" class="carousel slide carousel-fade" data-ride="carousel">
-                <div class="carousel-inner">
-                    <div class="carousel-item active">
-                        <img src="images/cuteblouse.jpeg" class="d-block w-100 imgCaro" alt="cutetop.jpg">
+                <!-- <form class="form-inline">
+
+                    <div class="input-group mb-2 mr-sm-2 col-md-offset-9">
+
+                        <div class="input-group-append">
+
+                            <div class="input-group-text"><i class="fa fa-search"></i></div>
+
+                        </div>
+
+                        <input type="text" class="form-control" id="searchClothes" placeholder="Search">
+
                     </div>
-                    <div class="carousel-item">
-                        <img src="images/trouser.jpeg" class="d-block w-100 imgCaro" alt="cutetop.jpg">
-                    </div>
-                    <div class="carousel-item">
-                        <img src="images/shoe.jpeg" class="d-block w-100 imgCaro" alt="cutetop.jpg">
-                    </div>
-                </div>
-                <a class="carousel-control-prev" href="#carouselFade" role="button" data-slide="prev">
-                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                    <span class="sr-only">Previous</span>
-                </a>
-                <a class="carousel-control-next" href="#carouselFade" role="button" data-slide="next">
-                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                    <span class="sr-only">Next</span>
-                </a>
+
+                </form> -->
             </div>
         </div>
-        <div class="col-md-3 sidebar">
+        <div class="row">
+            <div class="col-md-9 profile">
 
-            <h4> Categories </h4>
+                <nav class="menu" tabindex="0">
+                    <div class="smartphone-menu-trigger"></div>
+                        <header class="avatar">
 
-            <?php
-            
-            include('sidebar.php');
-            
-            ?>
-            
+                            <img src="images/musmus.jpg" />
+
+                            <h2 style="font-family:'Sacramento', cursive;"> Musangi  </h2>
+
+                        </header>
+
+                        <ul>
+                            <li tabindex="0" ><span>Dashboard </span><i class="fa fa-dashboard"></i></li>
+                            <li tabindex="0" ><span>Explore </span><i class="fa fa-search"></i></li>
+                            <li tabindex="0" ><span>Favorites </span><i class="fa fa-heart"></i></li>
+                            <li tabindex="0" ><span>Popular </span><i class="fa fa-star"></i></li>
+                            <li tabindex="0" ><span>Log Out </span><i class="fa fa-sign-out"></i></li>
+                        </ul>
+                </nav>
+
+                <div class="profileviews">
+
+                    <div class="col-md-4">
+
+                        <div class="container">
+
+                            <div class="box">
+
+                                <div id="favourites">
+
+                                    <h3 style="">Favourites</h3>
+
+                                    <img src="images/cuteblouse.jpeg" alt="" class="img-responsive">
+
+                                </div>
+
+                            </div>
+
+                        </div>
+
+                    </div>
+
+                </div>
+
+            </div>
+
+
+            <!-- <div class="col-md-3">
+                <div id="carouselFade" class="carousel slide carousel-fade" data-ride="carousel">
+                    <div class="carousel-inner">
+                        <div class="carousel-item active">
+                            <img src="images/cuteblouse.jpeg" class="d-block w-100 imgCaro" alt="cutetop.jpg">
+                        </div>
+                        <div class="carousel-item">
+                            <img src="images/trouser.jpeg" class="d-block w-100 imgCaro" alt="cutetop.jpg">
+                        </div>
+                        <div class="carousel-item">
+                            <img src="images/shoe.jpeg" class="d-block w-100 imgCaro" alt="cutetop.jpg">
+                        </div>
+                    </div>
+                    <a class="carousel-control-prev" href="#carouselFade" role="button" data-slide="prev">
+                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                        <span class="sr-only">Previous</span>
+                    </a>
+                    <a class="carousel-control-next" href="#carouselFade" role="button" data-slide="next">
+                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span class="sr-only">Next</span>
+                    </a>
+                </div>
+            </div> -->
+            <div class="col-md-offset-10 sidebar">
+
+                <h4 style="color:grey;"> Categories </h4>
+
+                <?php
+                
+                include('sidebar.php');
+                
+                ?>
+                
+            </div>
         </div>
+
     </div>
 
 
--- sidebar.php
+++ sidebar.php
@@ -1,1 +1,9 @@
-hello
+
+
+    <ul class="side"> <!---->
+        <li><a href="#">Jackets</a></li>
+        <li><a href="#">Hoodies</a></li>
+        <li><a href="#">Pants</a></li>
+        <li><a href="#">Shoes</a></li>
+    </ul> <!---->
+
