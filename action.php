86f0e7c02ed3980489f0760242370419c2e88b38
musangisilvia <musangisilvia@gmail.com>
2020-12-09 18:49:59 +0300

    action page added: where all guests to site land

---
+++ action.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!--Bootstrap CSS CDN-->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+    <!--FontAwesome CDN-->
+    <script src="https://use.fontawesome.com/10bafe97be.js"></script>
+
+    <!--Google Fonts CDN-->
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Comfortaa&family=Sacramento&display=swap" rel="stylesheet">
+
+    <!--CSS-->
+    <link rel="stylesheet" href="css/styleaction.css">
+
+
+    <title>Vazzi | Clothes Recommendation</title>
+    <link rel="shortcut icon" href="images/logo.ico" type="image/x-icon">
+</head>
+<body >
+
+    <header>
+        <div class="col-md-12 logo">
+            
+            <nav class="navbar navbar-light bg-light">
+                <a class="navbar-brand"style="font-family:'Sacramento', cursive; color:grey;font-size:40px; "> Vazzi </a>
+                <a href="login.php" class="navbar-brand"style="font-family:'Sacramento', cursive; color:grey;font-size:26px; float: right; "> Sign In | Sign Up </a>
+                <form class="form-inline">
+                    <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+                    <button alt="Search" class="btn btn-outline-dark my-2 my-sm-0" type="submit"><i class="fa fa-search"></i></button>
+                </form>
+            </nav>
+        
+        </div>
+    </header>
+    
+</body>
+</html>
