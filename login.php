5d59d16fdd51011770facc826b5f83194ac2aa05
musangisilvia <musangisilvia@gmail.com>
2020-12-29 12:26:38 +0300

    Using external php file to validate login

--- login.php
+++ login.php
@@ -27,7 +27,7 @@
 
             <h2>Log In</h2>
 
-            <form method="post" action="index.php"> <!--log in form start!-->
+            <form method="post" action="login_validate.php" enctype="multipart/form-data"> <!--log in form start!-->
 
                 <div class="col-auto">
 
@@ -82,47 +82,6 @@
             </form>
         </div>
     </div>
-
-    <?php
-            
-    include('includes/db.php');
-
-    if(isset($_POST['login'])){
-
-        $email = $_POST['email'];
-
-        $password = $_POST['password'];
-
-        if(empty($email) || empty($password)) {
-
-            echo "<script>alert('Fill out all fields! ')</script>";
-
-        } else {
-
-            $get_user = "SELECT * FROM user WHERE user_email = '$email' AND user_password = '$password'";
-
-            $run_user = mysqli_query($con,$get_user);
-
-            $check_user = mysqli_num_rows($run_user);
-
-            if($check_user==1){
-
-                $_SESSION['email'] = $email;
-
-                echo "<script>alert('Welcome, you have logged in.')</script>";
-
-                echo "<script>window.open('index.php','_self')</script>";
-
-            } else {
-
-                echo "<script>alert('Incorrect email or password')</script>";
-            }
-
-        }
-         
-    }
-
-?>
 
 
 </body>
---
+++ login_validate.php
@@ -0,0 +1,40 @@
+<?php
+            
+            include('includes/db.php');
+        
+            if(isset($_POST['login'])){
+        
+                $email = $_POST['email'];
+        
+                $password = $_POST['password'];
+        
+                if(empty($email) || empty($password)) {
+        
+                    echo "<script>alert('Fill out all fields! ')</script>";
+        
+                } else {
+        
+                    $get_user = "SELECT * FROM user WHERE user_email = '$email' AND user_password = '$password'";
+        
+                    $run_user = mysqli_query($con,$get_user);
+        
+                    $check_user = mysqli_num_rows($run_user);
+        
+                    if($check_user==1){
+        
+                        $_SESSION['email'] = $email;
+        
+                        echo "<script>alert('Welcome, you have logged in.')</script>";
+        
+                        echo "<script>window.open('index.php','_self')</script>";
+        
+                    } else {
+        
+                        echo "<script>alert('Incorrect email or password')</script>";
+                    }
+        
+                }
+                 
+            }
+        
+        ?>
